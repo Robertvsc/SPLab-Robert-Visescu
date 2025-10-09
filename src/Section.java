4fc63f6e8a27bf0fe87f39b496ed79b51fca84ac
Robertvsc <rvisescu77@gmail.com>
2025-10-09 14:07:23 +0300

    Clasa Section

---
+++ src/Section.java
@@ -0,0 +1,35 @@
+//Composite Section.java
+import java.util.ArrayList;
+import java.util.List;
+
+public class Section implements Element {
+    private String title;
+    private List<Element> elements = new ArrayList<>();
+
+    public Section(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Section: " + title);
+        for (Element e : elements) {
+            e.print();
+        }
+    }
+
+    @Override
+    public void add(Element element) {
+        elements.add(element);
+    }
+
+    @Override
+    public void remove(Element element) {
+        elements.remove(element);
+    }
+
+    @Override
+    public Element get(int index) {
+        return elements.get(index);
+    }
+}
