26e2b187db7b8c5ca3b746f543c100412abde298
Robertvsc <rvisescu77@gmail.com>
2025-10-11 16:10:49 +0300

    modificari pentru a fi elemente

--- src/Image.java
+++ src/Image.java
@@ -1,12 +1,19 @@
-// Image.java
-public class Image {
-    private String imageName;
+public class Image implements Element {
+    private String url;
 
-    public Image(String imageName) {
-        this.imageName = imageName;
+    public Image(String url) {
+        this.url = url;
     }
 
+    @Override
     public void print() {
-        System.out.println("Image: " + imageName);
+        System.out.println("Image: " + url);
     }
+
+    @Override
+    public void add(Element element) {}
+    @Override
+    public void remove(Element element) {}
+    @Override
+    public Element get(int index) { return null; }
 }
--- src/Paragraph.java
+++ src/Paragraph.java
@@ -1,11 +1,19 @@
-public class Paragraph {
+public class Paragraph implements Element {
     private String text;
 
     public Paragraph(String text) {
         this.text = text;
     }
 
+    @Override
     public void print() {
         System.out.println("Paragraph: " + text);
     }
+
+    @Override
+    public void add(Element element) {}
+    @Override
+    public void remove(Element element) {}
+    @Override
+    public Element get(int index) { return null; }
 }
