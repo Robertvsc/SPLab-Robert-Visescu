a8e68ed3c3b2f78cca38d209d75bc5f8b729bf6e
Robertvsc <rvisescu77@gmail.com>
2025-10-15 17:40:24 +0300

    ultima clasa modificata dupa structura

--- src/TableOfContents.java
+++ src/TableOfContents.java
@@ -1,6 +1,13 @@
-// TableOfContents.java
-public class TableOfContents {
+public class TableOfContents implements Element {
+    @Override
     public void print() {
-        System.out.println("Table of Contents...");
+        System.out.println("table of contents");
     }
+
+    @Override
+    public void add(Element element) {}
+    @Override
+    public void remove(Element element) {}
+    @Override
+    public Element get(int index) { return null; }
 }
