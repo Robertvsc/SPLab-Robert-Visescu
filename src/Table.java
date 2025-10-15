69bf5beece1b2dcb1de57a82cf48645733c223a0
Robertvsc <rvisescu77@gmail.com>
2025-10-15 17:31:44 +0300

    clasa table modificata

--- src/Table.java
+++ src/Table.java
@@ -1,12 +1,19 @@
-// Table.java
-public class Table {
+public class Table implements Element {
     private String title;
 
     public Table(String title) {
         this.title = title;
     }
 
+    @Override
     public void print() {
         System.out.println("Table: " + title);
     }
+
+    @Override
+    public void add(Element element) {}
+    @Override
+    public void remove(Element element) {}
+    @Override
+    public Element get(int index) { return null; }
 }
